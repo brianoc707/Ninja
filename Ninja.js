a8e9731d6b8f48f2e64bebaced27c5fe5b1aac83
Brian O’Connor <THABOSSMAN@Brians-MacBook-Pro.local>
2020-01-07 13:01:47 -0500

    added inherited sensei class

--- Ninja.js
+++ Ninja.js
@@ -14,6 +14,8 @@ class Ninja {
         console.log(this.health);
         console.log(this.speed);
         console.log(this.strength);
+        console.log(this.wisdom);
+        
     }
     drinkSake(){
         this.health += 10;
@@ -21,6 +23,24 @@ class Ninja {
     }
 }
 
-const ninja1 = new Ninja("Brian", 100);
-ninja1.drinkSake();
-ninja1.showStats();
+class Sensei extends Ninja{
+    constructor(name){
+     super(name, 200);
+     this.wisdom = 10;
+     this.speed = 10; 
+     this.strength = 10;
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+const Sensei1 = new Sensei("Bernard");
+Sensei1.speakWisdom();
+Sensei1.showStats();
+
+
+// const ninja1 = new Ninja("Brian", 100);
+// ninja1.drinkSake();
+// ninja1.showStats();
